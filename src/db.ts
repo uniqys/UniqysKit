4d6865f85cee519b0447f20c8b9b96eec8dff029
nekobato <nekobato@gmail.com>
2018-03-13 12:36:02 +0900

    use semaphore

--- src/db.ts
+++ src/db.ts
@@ -1,11 +1,59 @@
-import * as levelup from 'levelup'
-import * as memdown from 'memdown'
+import levelup from 'levelup'
+import memdown from 'memdown'
+import semaphore, { Semaphore } from 'semaphore'
+import { resolve } from 'path'
 
-class Database {
+interface IDatabaseOps {
+  db: string,
+  type: string,
+  key: string,
+  keyEncoding?: string,
+  value: string,
+  valueEncoding?: string
+}
 
-  blockDB: any
+export class Block {
+  hash (): string {
+    return 'test-hash'
+  }
+  serialize (): string {
+    return 'test-serialize'
+  }
+}
+
+export class Database {
+  blockDatabase: any
+  blockDatabaseOps: Array<IDatabaseOps>
+  semaphore: Semaphore
 
   constructor (options: any) {
-    this.blockDB = options.blockDB ? options.blockDB : levelup(memdown())
+    this.blockDatabase = options.database ? options.database : levelup(memdown())
+    this.blockDatabaseOps = []
+    this.semaphore = semaphore(1)
+  }
+
+  public putBlock (block: Block): Promise<void> {
+    return new Promise((resolve) => {
+      this.semaphore.take(() => {
+        this.blockDatabaseOps.push({
+          db: 'block',
+          type: 'put',
+          key: block.hash(),
+          value: block.serialize(),
+          valueEncoding: 'binary'
+        })
+        this.batchDatabaseOps()
+        this.semaphore.leave()
+        return resolve()
+      })
+    })
+  }
+
+  public getBlock (hash: string): Promise<Buffer> {
+    return this.blockDatabase.get(hash)
+  }
+
+  private batchDatabaseOps (): Promise<any> {
+    return this.blockDatabase.batch(this.blockDatabaseOps)
   }
 }
