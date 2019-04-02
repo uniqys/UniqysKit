6d90e7e8591eb2f212225e2140a88529e9017783
kita <kita@mfac.jp>
2019-04-02 18:47:34 +0900

    fix(easy-framework): fix self transfer bug

--- packages/easy-framework/src/api.ts
+++ packages/easy-framework/src/api.ts
@@ -186,12 +186,15 @@ export class InnerApi extends OuterApi {
         ctx.assert(fromAddr, 400)
         ctx.assert(toAddr, 400)
         ctx.assert(value && typeof (value as any) === 'number', 400)
-        await this.mutex.use(async () => {
-          const fromAccount = await this.state.getAccount(fromAddr!)
-          const toAccount = await this.state.getAccount(toAddr!)
-          await this.state.setAccount(fromAddr!, fromAccount.decreaseBalance(value))
-          await this.state.setAccount(toAddr!, toAccount.increaseBalance(value))
-        })
+
+        if (fromAddr! !== toAddr!) {
+          await this.mutex.use(async () => {
+            const fromAccount = await this.state.getAccount(fromAddr!)
+            const toAccount = await this.state.getAccount(toAddr!)
+            await this.state.setAccount(fromAddr!, fromAccount.decreaseBalance(value))
+            await this.state.setAccount(toAddr!, toAccount.increaseBalance(value))
+          })
+        }
         ctx.status = 200
       })
     this.use()
